8a150c5407676bf2009bddeab9ae8e80e42bc70b
Gleb Nikishin <gleb.nikishin.2003@gmail.com>
2023-02-21 17:33:10 +0300

    calculate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello " + args[0]);
+
+        System.out.print("x = ");
+        double x = scanner.nextDouble();
+        System.out.print("y = ");
+        double y = scanner.nextDouble();
+        System.out.println((3.0 * Math.pow(Math.cos(x - (Math.PI / 6.0)), 2.0)) / (0.5 + Math.sin(Math.pow(y, 2.0))));
     }
 }
